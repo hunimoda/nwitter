9425e447af386689a7e11e48244137fdd43f2ff7
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-13 08:55:22 +0900

    #2.1 Login Form part One

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,56 @@
+import { useState } from "react";
+
 const Auth = () => {
-	return <span>Auth</span>;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const changeHandler = (event) => {
+		const {
+			target: { name, value },
+		} = event;
+
+		switch (name) {
+			case "email":
+				setEmail(value);
+				break;
+			case "password":
+				setPassword(value);
+				break;
+			default:
+		}
+	};
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+	};
+
+	return (
+		<div>
+			<form onSubmit={submitHandler}>
+				<input
+					name="email"
+					type="email"
+					placeholder="Email"
+					required
+					value={email}
+					onChange={changeHandler}
+				/>
+				<input
+					name="password"
+					type="password"
+					placeholder="Password"
+					required
+					value={password}
+					onChange={changeHandler}
+				/>
+				<button>Log In</button>
+			</form>
+			<div>
+				<button>Continue with Google</button>
+				<button>Continue with Github</button>
+			</div>
+		</div>
+	);
 };
 
 export default Auth;
