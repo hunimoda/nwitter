c8dde8c5a6f83bbe0d45fa09a683a40974b80d2a
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-13 12:05:42 +0900

    #3.1 Nweeting!

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/database";
+import "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBUm_UrDh7vjdZSdnI0tJpi5oVKzIF25uY",
@@ -14,3 +14,4 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
