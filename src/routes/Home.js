554fe3a50fa097543eb995fc2760aab727bb19e4
Dae Hoon Kim <hunimoda@gmail.com>
2022-01-13 11:54:54 +0900

    #3.0 Form and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const Home = () => {
-	return <span>Home</span>;
+	const [nweet, setNweet] = useState("");
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+	};
+
+	const onChange = (event) => {
+		const {
+			target: { value },
+		} = event;
+
+		setNweet(value);
+		console.log(value);
+	};
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input
+					value={nweet}
+					onChange={onChange}
+					type="text"
+					placeholder="What's on your mind?"
+					maxLength={120}
+				/>
+				<button>Nweet</button>
+			</form>
+		</div>
+	);
 };
 
 export default Home;
